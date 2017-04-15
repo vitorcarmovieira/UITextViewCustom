290b917b32cc69c90ff3c4d1cd8065df0f0adb9c
Vitor Vieira <vitorcarmovieira@gmail.com>
2017-04-15 15:01:01 -0400

    adicionado placeholder

---
+++ UITextViewCustom/TextViewCustom.h
@@ -0,0 +1,18 @@
+//
+//  TextViewCustom.h
+//  UITextViewCustom
+//
+//  Created by Vitor Vieira on 15/04/17.
+//  Copyright © 2017 Vitor Vieira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+IB_DESIGNABLE
+@interface TextViewCustom : UITextView
+
+@property (nonatomic, retain) IBInspectable NSString *placeholder;
+@property (nonatomic, retain) IBInspectable UIColor *placeholderColor;
+
+-(void)textChanged:(NSNotification*)notification;
+
+@end
