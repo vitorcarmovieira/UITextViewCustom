cfae4b9b823f8395f1400c6b2217136d59f1b72c
Vitor Vieira <vitorcarmovieira@gmail.com>
2017-04-15 14:44:26 -0400

    Initial Commit

---
+++ UITextViewCustom/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITextViewCustom
+//
+//  Created by Vitor Vieira on 15/04/17.
+//  Copyright © 2017 Vitor Vieira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITextViewCustom/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITextViewCustom
+//
+//  Created by Vitor Vieira on 15/04/17.
+//  Copyright © 2017 Vitor Vieira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
